459fe1753fb00a6b9c8f6a994c251b41faa33dd6
Santiago <svera1213@gmail.com>
2024-01-25 23:40:20 +1000

    Initial Commit

---
+++ TStocksIpad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TStocksIpad
+//
+//  Created by Santiago Vera Yanez on 1/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TStocksIpad/TStocksIpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TStocksIpadApp.swift
+//  TStocksIpad
+//
+//  Created by Santiago Vera Yanez on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TStocksIpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
